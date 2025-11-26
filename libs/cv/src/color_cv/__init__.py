b1ce742f6a881a6a32c045ef637f3f5bbee96cee
hatim2005 <156291487+hatim2005@users.noreply.github.com>
2025-11-27 01:58:52 +0530

    Initialize color_cv module with version and exports

---
+++ libs/cv/src/color_cv/__init__.py
@@ -0,0 +1,11 @@
+"""Color CV Library - Computer Vision Algorithms for Color Correction"""
+
+from .aruco_detector import ArucoDetector, CardDetectionResult, CardOrientation, PatchROI
+
+__version__ = "1.0.0"
+__all__ = [
+    "ArucoDetector",
+    "CardDetectionResult",
+    "CardOrientation",
+    "PatchROI",
+]
