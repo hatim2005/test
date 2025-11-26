277faac3359df3d846f0061815ded382e048553a
hatim2005 <156291487+hatim2005@users.noreply.github.com>
2025-11-27 02:33:22 +0530

    Add white balance and color correction classes

--- libs/cv/src/color_cv/__init__.py
+++ libs/cv/src/color_cv/__init__.py
@@ -1,6 +1,9 @@
 """Color CV Library - Computer Vision Algorithms for Color Correction"""
 
 from .aruco_detector import ArucoDetector, CardDetectionResult, CardOrientation, PatchROI
+from .white_balance import WhiteBalancer, WhiteBalanceMethod
+from .ccm import ColorCorrectionMatrix
+from .delta_e import DeltaECalculator
 
 __version__ = "1.0.0"
 __all__ = [
@@ -8,4 +11,8 @@ __all__ = [
     "CardDetectionResult",
     "CardOrientation",
     "PatchROI",
+    "WhiteBalancer",
+    "WhiteBalanceMethod",
+    "ColorCorrectionMatrix",
+    "DeltaECalculator",
 ]
